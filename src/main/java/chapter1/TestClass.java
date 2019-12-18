fd3d373844d346d20f771b1bb6a5dbd8b4389613
nageshdv1990 <2nageshdv@gmail.com>
2019-12-18 23:04:53 +0530

    Initial Commit: Get working

---
+++ src/main/java/chapter1/TestClass.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class TestClass {
+    public static void main(String[] args){
+        ZipGet zipGet1 = new ZipGet();
+        zipGet1.requestUsZipcode90210_checkPlaceNameInResponseBody_expectBeverlyHills();
+
+    }
+}
---
+++ src/main/java/chapter1/ZipGet.java
@@ -0,0 +1,26 @@
+package chapter1;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class ZipGet {
+    @Test
+    public void requestUsZipcode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
+                given().
+                        log().all().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+                then().
+                        log().body().
+                        assertThat().body("places[0].'place name'", equalTo("Beverly Hills")).//gpath is used to identify the element and hamcrest matcher validates (equalTo, hasItem, hasSize, not(equalTo(X))
+                        assertThat().statusCode(200).
+                        assertThat().contentType(ContentType.JSON);
+       // System.out.println(get("http://api.zippopotam.us/us/90210"));
+
+    }
+
+}
