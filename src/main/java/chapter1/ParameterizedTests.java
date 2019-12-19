0207301f3a11d6d318e10fde5ef1b5e15fc66a53
nageshdv1990 <2nageshdv@gmail.com>
2019-12-19 12:24:33 +0530

    ParameterizedTests

---
+++ src/main/java/chapter1/ParameterizedTests.java
@@ -0,0 +1,40 @@
+package chapter1;
+
+import com.tngtech.java.junit.dataprovider.*;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(DataProviderRunner.class)
+public class ParameterizedTests {
+
+    @DataProvider
+    public static Object[][] zipCodesAndPlaces(){
+        return new  Object[][]{
+                {"us","90210","Beverly Hills"},
+                {"us","12345","Schenectady"},
+                {"ca","B2R","Waverley"}
+        };
+
+    }
+
+
+    @Test
+    @UseDataProvider("zipCodesAndPlaces")
+    public void parameterizedTests_positiveTests_expected200(String countryCode, String zipCode, String location) {
+            given().
+                pathParam("countryCode", countryCode).pathParam("zipCode",zipCode).
+                log().all().
+            when().
+                get("http://api.zippopotam.us/{countryCode}/{zipCode}").
+            then().
+                log().body().
+                assertThat().contentType(ContentType.JSON).
+                assertThat().statusCode(200).
+                assertThat().body("places[0].'place name'",equalTo(location));
+
+    }
+}
