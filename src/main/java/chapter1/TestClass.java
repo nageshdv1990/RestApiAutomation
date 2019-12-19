32a6e2c8dd8e38a77eeb7282e7d21446f4e0c7e0
nageshdv1990 <2nageshdv@gmail.com>
2019-12-19 15:15:09 +0530

    Implemented: 1. Request Spec 2. Response Spec 3. Extract function

--- src/main/java/chapter1/TestClass.java
+++ src/main/java/chapter1/TestClass.java
@@ -2,8 +2,9 @@ package chapter1;
 
 public class TestClass {
     public static void main(String[] args){
-        ZipGet zipGet1 = new ZipGet();
-        zipGet1.requestUsZipcode90210_checkPlaceNameInResponseBody_expectBeverlyHills();
-
+       // ZipGet zipGet1 = new ZipGet();
+        //zipGet1.requestUsZipcode90210_checkPlaceNameInResponseBody_expectBeverlyHills();
+       // ParameterizedTests pt1=new ParameterizedTests();
+       // pt1.parameterizedTests_positiveTests_expected200();
     }
 }
